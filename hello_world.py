b4eb9391e561a09279b5ac84c0f0288c50605a10
woojin134 <woojin3582@gmail.com>
2023-09-14 11:18:16 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,8 @@
 def print_hello():
     print("Hello World!!")
+def print_KIT():
+    print("Kumoh National Institue of Technology")
 
 if __name__ == '__main__':
     print_hello()
-    
+    print_KIT
